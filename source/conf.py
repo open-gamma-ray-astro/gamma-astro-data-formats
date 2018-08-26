e76db1182f659dad06fa5746a0b74e7c42c834db
Christoph Deil <Deil.Christoph@gmail.com>
2018-08-26 19:16:15 +0200

    Improve Sphinx PDF output

--- source/conf.py
+++ source/conf.py
@@ -220,7 +220,8 @@ htmlhelp_basename = 'gadf'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
+'extraclassoptions': 'openany',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
@@ -246,7 +247,8 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+# latex_use_parts = False
+latex_toplevel_sectioning = 'chapter'
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
